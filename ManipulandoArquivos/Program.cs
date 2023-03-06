afc1f6efe52bc72d034bc7e523c962bdffd04506
Idevaldo Urias <idevaldorodrigues@gmail.com>
2023-03-06 14:33:16 -0300

    Fazendo uso do método File.OpenText() para instanciar dentro do bloco using um objeto do tipo StreamReader, e posteriormente imprimir linha por linha, tratando possíveis exceções com a classe IOExcepetion.

--- ManipulandoArquivos/Program.cs
+++ ManipulandoArquivos/Program.cs
@@ -11,18 +11,16 @@ namespace ManipulandoArquivos
 
             try
             {
-                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    using (StreamReader sr = new StreamReader(fs))
+                    while (!sr.EndOfStream)
                     {
-                        while (!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            Console.WriteLine(line);
-                        }
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
                     }
                 }
-            } catch (IOException e)
+            }
+            catch (IOException e)
             {
                 Console.WriteLine("An error ocurred: ");
                 Console.WriteLine(e.Message);
