5aeeca2db4d0c8911b951a9d011e4b548aa44422
Idevaldo Urias <idevaldorodrigues@gmail.com>
2023-03-06 14:26:55 -0300

    Utilizando o bloco using para fechar de forma automática as classes FileStream e StreamReader

--- ManipulandoArquivos/Program.cs
+++ ManipulandoArquivos/Program.cs
@@ -9,28 +9,18 @@ namespace ManipulandoArquivos
         {
             string path = @"c:\temp\file1.txt";
 
-            FileStream fs = null;
-            StreamReader sr = null;
-
-            try
+            using (FileStream fs = new FileStream(path, FileMode.Open))
             {
-                sr = File.OpenText(path);
-
-                while (!sr.EndOfStream)
+                using (StreamReader sr = new StreamReader(fs))
                 {
-                    string line = sr.ReadLine();
-
-                    Console.WriteLine(line);
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
                 }
-
-            } catch (IOException ex)
-            {
-                Console.WriteLine("An error acurred: ");
-                Console.WriteLine(ex.Message);
-            } finally
-            {
-                if (sr != null) sr.Close();
             }
+
         }
     }
 }
